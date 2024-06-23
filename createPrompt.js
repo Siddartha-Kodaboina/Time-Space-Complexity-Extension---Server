014a0e42094913f873330531fab2cff2105d2b71
Siddartha Kodaboina <k.siddartha49@gmail.com>
2024-06-23 16:52:57 -0700

    Added more examples in the prompt

--- createPrompt.js
+++ createPrompt.js
@@ -17,9 +17,8 @@ const createPrompt = (selectedText) => {
     complexity of the code just like below in the same format, please do not explain anything
 
     Example1:
-        CODE: 
-        bool findElement(int arr[], int n, int key)
-        {
+    CODE: 
+        bool linearSearch(int arr[], int n, int key) {
             for (int i = 0; i < n; i++) {
                 if (arr[i] == key) {
                     return true;
@@ -27,10 +26,10 @@ const createPrompt = (selectedText) => {
             }
             return false;
         }
-    Time:O(N)|Space:(N)
+    Time:O(N)|Space:(1)
 
     Example2:
-        CODE: 
+    CODE: 
         int binarySearch(int arr[], int l, int r, int x)
         {
             if (r >= l) {
@@ -43,10 +42,10 @@ const createPrompt = (selectedText) => {
             }
             return -1;
         }
-    Time:O(log(N))|Space:(N)
+    Time:O(log N)|Space:(1)
 
     Example3:
-        CODE: 
+    CODE: 
         void bubbleSort(int arr[], int n)
         {
             for (int i = 0; i < n - 1; i++) {
@@ -57,16 +56,157 @@ const createPrompt = (selectedText) => {
                 }
             }
         }
-    Time:O(N**2)|Space:(N)
+    Time:O(N**2)|Space:(1)
 
     Example4:
-        void iterateLoop(){
-        for (int i = 1; i < n; i++) {
-            i *= k;
+    Code:
+        void merge(int arr[], int l, int m, int r) {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+            int L[n1], R[n2];
+            for (int i = 0; i < n1; i++)
+                L[i] = arr[l + i];
+            for (int j = 0; j < n2; j++)
+                R[j] = arr[m + 1 + j];
+            int i = 0, j = 0, k = l;
+            while (i < n1 && j < n2) {
+                if (L[i] <= R[j]) {
+                    arr[k] = L[i];
+                    i++;
+                } else {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+            while (i < n1) {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+            while (j < n2) {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
         }
+
+        void mergeSort(int arr[], int l, int r) {
+            if (l >= r)
+                return;
+            int m = l + (r - l) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
         }
     
-    Time:O(log(N) base K)|Space:(1)
+    Time:O(N log(N))|Space:O(N)
+
+    Example 5:
+    Code:
+        int fibonacci(int n) {
+            if (n <= 1)
+                return n;
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    Time:O(2**N)|Space:O(N)
+
+    Example 6:
+    Code:
+        int tsp(int graph[][4], vector<int> &path, int pos, int n, int count, int cost, int &ans) {
+            if (count == n && graph[pos][0]) {
+                ans = min(ans, cost + graph[pos][0]);
+                return ans;
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (!path[i] && graph[pos][i]) {
+                    path[i] = true;
+                    tsp(graph, path, i, n, count + 1, cost + graph[pos][i], ans);
+                    path[i] = false;
+                }
+            }
+
+            return ans;
+        }
+
+        int main() {
+            int n = 4;
+            int graph[][4] = {
+                {0, 10, 15, 20},
+                {10, 0, 35, 25},
+                {15, 35, 0, 30},
+                {20, 25, 30, 0}
+            };
+
+            vector<int> path(n, 0);
+            path[0] = true;
+            int ans = INT_MAX;
+
+            cout << tsp(graph, path, 0, n, 1, 0, ans) << endl;
+
+            return 0;
+        }
+    Time:O(N!)|Space:O(N)
+
+    Here are some tricky cases
+    Tricky Example 1:
+    Code:
+        void trickyLoop(int n) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i + j == n - 1)
+                        break;
+                }
+            }
+        }
+    Time:O(N**2)|Space:O(1)
+
+    Tricky Example 2:
+    Code:
+        int trickyRecursion(int n) {
+            if (n <= 1)
+                return n;
+            return trickyRecursion(n - 1) + trickyRecursion(n - 3);
+        }
+
+    Time:O(2**(N/3))|Space:O(N)
+
+    Tricky Example 3:
+    Code:
+        void decrementIncrementLoop(int n) {
+            for (int i = n; i > 0; i /= 2) {
+                for (int j = 0; j < i; j++) {
+                    // Some operation
+                }
+            }
+        }
+    Time:O(N)|Space:O(1)
+
+    Tricky Example 4:
+    Code:
+        void multipleRecursion(int n) {
+            if (n <= 0)
+                return;
+            if (n % 2 == 0)
+                multipleRecursion(n / 2);
+            else
+                multipleRecursion(n - 1);
+        }
+    Time:O(log N)|Space:O(log N)
+
+    Tricky Example 5:
+    Code:
+        void changingStepsLoop(int n) {
+            for (int i = 1; i < n; i *= 3) {
+                for (int j = 0; j < i; j++) {
+                    // Some operation
+                }
+            }
+        }
+    Time:O(N)|Space:O(1)
+
+    
 
     Please generate time snad space complexities as the format mentioned inside the  <output-format> laterals, please do not print any other context
 
