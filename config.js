d7c61886dfa862f45f385f3c0f2c41d2b03941d8
Siddartha Kodaboina <k.siddartha49@gmail.com>
2024-09-20 03:24:57 -0700

    Changing LLM to GPT-4o from gemini-1.5-flash

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
 const API_KEY = process.env.API_KEY;
-export default API_KEY;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+export {API_KEY, OPENAI_API_KEY};
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { GoogleGenerativeAI } from '@google/generative-ai';
-import createPrompt from './createPrompt.js'; // Import the createPrompt function
-import API_KEY from './config.js';
+import OpenAI from 'openai';
+import createPrompt from './createPrompt.js'; 
+import {API_KEY, OPENAI_API_KEY} from './config.js';
 import cors from 'cors';
 
 
@@ -12,19 +13,33 @@ const port = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
+// OpenAI Configuration
+const openai = new OpenAI({
+  apiKey: OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true 
+});
+const model = "gpt-4o";
+
 app.post('/analyze', async (req, res) => {
-  const { selectedText } = req.body;
+  let { selectedText } = req.body;
+  selectedText = cleanSelectedText(selectedText);
   
   // Create the prompt
   const prompt = createPrompt(selectedText);
 
   try {
-    const genAI = new GoogleGenerativeAI(API_KEY);
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const response = await openai.chat.completions.create({
+      model: model,
+      messages: [{ role: "user", content: prompt }],
+    });
+    const responseText = response.choices[0].message.content;
 
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    const responseText = await response.text();
+    // const genAI = new GoogleGenerativeAI(API_KEY);
+    // const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
+    // const result = await model.generateContent(prompt);
+    // const response = await result.response;
+    // const responseText = await response.text();
     console.log("called analyze and repsonding with", responseText);
     res.json({ result: responseText });
   } catch (error) {
@@ -32,6 +47,26 @@ app.post('/analyze', async (req, res) => {
     res.status(500).json({ error: 'Error analyzing the code' });
   }
 });
+
+function allDigits(line){
+  return /^\d+$/.test(line);
+}
+
+function cleanSelectedText(text) {
+  // Split text by a new line character
+  const textLines = text.split('\n');
+  var cleanedText = "";
+  for(let i = 0; i < textLines.length; i++){
+    const newLine = textLines[i].replace('·', ' ').trim(); // replace '·' with '' // // strip the spaces on both sides of new
+    if(allDigits(newLine) || newLine==''){
+      continue;
+    }
+    cleanedText += newLine + '\n';
+  }
+  console.log(cleanedText);
+  return cleanedText;
+
+}
 
 // Start the server
 app.listen(port, () => {
